27da23f963194f0c4c52a5d3aaa7d2b6ca0d8e84
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-17 12:43:08 +0530

    Added the User component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const AddUser = () => {
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+
+    }
   return (
-    <div>AddUser</div>
+    <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>UserName</label>
+        <input id="username" type="text" placeholder='Enter the username'/>
+        <label htmlFor='age'>Age</label>
+        <input id="age" type="number" placeholder='Enter the age'/>
+        <button type='submit'>Submit</button>
+    </form>
   )
 }
 
