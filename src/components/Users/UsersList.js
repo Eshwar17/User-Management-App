d3aadb0bc6d197d060158b0fc9ff890db889850c
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-17 15:11:44 +0530

    Added UsersList component in App component and assigned an empty array to the users prop

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Card from "../UI/Card";
-
+import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
   return (
-    <Card>
+    <Card className={classes.users}>
       <ul>
         {props.users.map((user) => {
-          <li>
+          return <li>
             {user.name} ({user.age} years!)
           </li>;
         })}
